0b3a2d4ce825df38419dfb497858ba751b9b72f8
Mark Rodriguez <mrkxvncnt@gmail.com>
2018-06-16 17:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,33 +2,29 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-shout('hello')
 
 function whisper(string) {
   return string.toLowerCase()
 }
 
-whisper("HELLO")
-
 function logShout(string) {
   console.log("HELLO")
 }
-
-logShout("hello")
 
 function logWhisper(string) {
   console.log("hello")
 }
 
-logWhisper("HELLO")
-
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase() return "YES INDEED!")
-  else if (string === string.toLowerCase() return "I can't hear you")
-  else if (string === ("I love you, Grandma.") return "I love you, too.")
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"}
+    
+  else if (string === string.toLowerCase()) { 
+    return "I can't hear you"}
 
+  else if ("I love you, Grandma.") {
+    return "I love you, too."}
 }
 
-sayHiToGrandma("hello")
-sayHiToGrandma("HELLO")
-sayHiToGrandma("I love you, Grandma.")
+
+
