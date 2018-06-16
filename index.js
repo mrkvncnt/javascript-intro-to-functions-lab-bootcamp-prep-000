bb4efccb3e659920b822746ed418da01fed7fd86
Mark Rodriguez <mrkxvncnt@gmail.com>
2018-06-16 17:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!"}
-    
-  else if (string === string.toLowerCase()) { 
+
+  else if (string === string.toLowerCase()) {
     return "I can't hear you"}
 
   else if ("I love you, Grandma.") {
     return "I love you, too."}
 }
 
-
-
+shout("hello")
+whisper("HELLO")
+logShout()
+logWhisper()
